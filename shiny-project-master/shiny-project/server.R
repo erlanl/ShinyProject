8ea9ab0c403bdc114bc2a32910ac610e50fb6da9
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2022-05-02 15:50:28 -0300

    Adicionando todos os gráficos

--- shiny-project-master/shiny-project/server.R
+++ shiny-project-master/shiny-project/server.R
@@ -7,6 +7,11 @@ server <- function(input, output) {
         stock_name <- input$stock
         twin <- input$true_date
         
+        master_df <- read.csv('amazon.csv', stringsAsFactors = T) %>%
+          group_by(Ano, Estado) %>%
+          summarise(Numero = sum(Numero, na.rm = T)) %>%
+          ungroup()
+        
         df_stock <- master_df %>% 
             filter(Estado == stock_name)
         
@@ -18,6 +23,9 @@ server <- function(input, output) {
         ano_final <- as.numeric(ano_final)
         
         ano_atual <- ano_inicial + 1
+        
+        #z <- df_stock %>% 
+          #filter(between(Ano, ano_inicial, ano_final))
         
         z <- df_stock %>% 
           filter(Ano == ano_inicial)
@@ -31,41 +39,95 @@ server <- function(input, output) {
           ano_atual <- ano_atual + 1
         }
     
-   
-        
-        master_df <- read.csv('amazon.csv', stringsAsFactors = T) %>%
-          group_by(Ano, Estado) %>%
-          summarise(Numero = sum(Numero, na.rm = T)) %>%
-          ungroup()
-
-        print(z)
         return(z)
     })
     
+    select_stock2 <- eventReactive(input$go, {
+      
+      stock_name <- input$stock
+      twin <- input$true_date
+      
+      #Recebendo os dados da tela
+      ano_inicial <- input$stock_ano_inicial
+      ano_final <- input$stock_ano_final
+      
+      #Convertendon para inteiro
+      ano_inicial <- as.numeric(ano_inicial)
+      ano_final <- as.numeric(ano_final)
+      
+      master_df <- read.csv('amazon.csv', stringsAsFactors = T)
+      
+      df_stock <- master_df %>% 
+        filter(Estado == stock_name) %>% 
+        filter(between(Ano, ano_inicial, ano_final))
+      
+      print(df_stock)
+      return(df_stock)
+    })
+    
+    select_2_stocks <- eventReactive(input$go_comp, {
+      
+      stock_name1 <- input$Estado1
+
+      stock_name2 <- input$Estado2
+
+      #Recebendo os dados da tela
+      ano_inicial <- input$stock_comp_ano_inicial
+      ano_final <- input$stock_comp_ano_final
+      #Convertendon para inteiro
+      ano_inicial <- as.numeric(ano_inicial)
+      ano_final <- as.numeric(ano_final)
+      
+      master1_df <- read.csv('amazon.csv', stringsAsFactors = T) %>%
+        group_by(Ano, Estado) %>%
+        summarise(Numero = sum(Numero, na.rm = T)) %>%
+        ungroup()
+  
+      
+      df1_stock <- master1_df %>% 
+        filter(Estado == stock_name1)
+      
+      df2_stock <- master1_df %>% 
+        filter(Estado == stock_name2)
+      
+      
+      z <- df1_stock %>% 
+      filter(between(Ano, ano_inicial, ano_final))
+
+      z1 <- df2_stock %>% 
+        filter(between(Ano, ano_inicial, ano_final))    
+      
+      print(z)
+      print(z1)
+      
+      tabela <- bind_rows(z, z1)
+      return(tabela)
+      
+    })
     
     ################ OUTPUT #####################
     Info_DataTable <- eventReactive(input$go,{
         df <- select_stock()
         
 
-        min <- min(df["Numero"])
-        max <- max(df["Numero"])
-        mean <- mean(df$Numero)
-        median <- median(df$Numero)
+        Min <- min(df["Numero"])
+        Max <- max(df["Numero"])
+        Média <- mean(df$Numero)
+        Mediana <- median(df$Numero)
         
         #obtem a tabela com frequencia das variaveis
         freq <- table(df["Numero"]);
         #obtem o nome da variavel que mais se repete
         moda <- names(table(df["Numero"]))[table(df["Numero"]) == max(table(df["Numero"]))]
-        moda <- as.numeric(moda)
+        Moda <- as.numeric(moda)
         
         variancia <- var(df$Numero)
-        desvio <- variancia^(1/2)
+        Desvio <- variancia^(1/2)
         
         
-        Stock <- input$stock
+        Estado <- input$stock
         
-        df_tb <-  data.frame(Stock, mean, moda, median, min, max, desvio)
+        df_tb <-  data.frame(Estado, Média, Moda, Mediana, Min, Max, Desvio)
         
         df_tb <- as.data.frame(t(df_tb))
 
@@ -83,23 +145,62 @@ server <- function(input, output) {
             ))
     })
     
-    #output$sh <- renderPlot({
-        ## All the inputs
-        #df <- select_stock()
+    output$hist <- renderPlot({
+        #All the inputs
+        df <- select_stock()
         
-        #aux <- df$Close %>% na.omit() %>% as.numeric()
-        #aux1 <- min(aux)
-        #aux2 <- max(aux)
+        num <- df$Numero %>% na.omit() %>% as.numeric()
+        num_min <- min(num)
+        num_max <- max(num)
         
-        #df$Date <- ymd(df$Date)
-        #a <- df %>% 
-        #    ggplot(aes(Date, Close, group=1)) +
-        #    geom_path() +
-        #    ylab('Preço da Ação em $') +
-        #    coord_cartesian(ylim = c(aux1, aux2)) +
-        #    theme_bw() +
-        #    scale_x_date(date_labels = "%Y-%m-%d")
-        
-        #a
-    #})
+        ggplot(df, aes(x = Numero)) +
+          geom_histogram(group = 1, binwidth = 100, color = "black", bins = 10000) + geom_bar(start = "count") + scale_x_continuous(limits = c(num_min, num_max), breaks = seq(0,num_max,500)) +
+          theme(axis.text = element_text(angle = 90, vjust = 0))
+
+    })
+    
+    output$sh <- renderPlot({
+      df <- select_stock()
+      
+      ano_limite <- df$Ano %>% na.omit() %>% as.numeric()
+      ano_limite_min <- min(ano_limite)
+      ano_limite_max <- max(ano_limite)
+      
+      num <- df$Numero %>% na.omit() %>% as.numeric()
+      num_min <- min(num)
+      num_max <- max(num)
+      
+      ggplot(df, aes(x= Ano, y=Numero)) +
+        geom_line(color = "red", size = 2)
+    })
+    
+    output$box <- renderPlot({
+      df <- select_stock2()
+      
+      ggplot(df, aes(x=as.factor(Ano), y=Numero)) + 
+        geom_boxplot(fill="slateblue", alpha=0.5) + 
+        xlab("Numero de Quimadas")
+    })
+    
+    output$grafo <- renderPlot({
+      df <- select_2_stocks()
+      print(df)
+      ggplot(df, aes(x= Ano, y=Numero, group = Estado, color = Estado)) +
+        geom_line(size = 2)
+      
+    })
+    
+    output$barra <- renderPlot({
+      df <- select_2_stocks()
+      
+      ggplot(df, aes(fill = Estado, y = Numero / 12, x = Ano)) +
+        geom_bar(position = "dodge", stat="identity")
+    })
+    
+    output$scat <- renderPlot({
+      df <- select_2_stocks()
+      
+      ggplot(df, aes(x = Ano, y = Numero, color = Estado)) +
+        geom_point(size = 6)
+    })
 }
--- shiny-project-master/shiny-project/ui.R
+++ shiny-project-master/shiny-project/ui.R
@@ -13,40 +13,49 @@ body <- dashboardBody(
     tabItems(
         tabItem(tabName = 'm',
                 fluidRow(
-                    box(title = 'Selecione suas opções', width=12, solidHeader = TRUE, status='warning',
-                        selectInput('stock', 'Ação', stock_list, multiple=FALSE),
+                    box(title = 'Selecione suas opções', width=12, solidHeader = TRUE, status = "primary",
+                        selectInput('stock', 'Estado', stock_list, multiple=FALSE),
                         selectInput('stock_ano_inicial', 'Ano Inicial', time_list, multiple=FALSE),
                         selectInput('stock_ano_final', 'Ano Final', time_list, multiple=FALSE),
-                        #uiOutput("timedate"),
                         actionButton('go', 'Submeter')
                         )
                 ),
                 fluidRow(
-                    box(title = "Informações sobre a ação", width = 12, solidHeader = TRUE,
+                    box(title = "Informações sobre o estado", width = 12, solidHeader = TRUE, status = "primary",
                         DTOutput('info')
 
                     )
                 ),
                 fluidRow(
-                    box(title = "Série de Preços", width = 12, solidHeader = TRUE,
-                        plotOutput('sh')
+                    box(title = "Queimadas ao longo dos anos", width = 12, solidHeader = TRUE, status = "primary",
+                        plotOutput('sh'),
+                        plotOutput('hist'),
+                        plotOutput('box')
                     )
                 ),
         ),
         tabItem(tabName = 'comp',
                 fluidRow(
-                    box(title = 'Selecione suas opções', width=12, solidHeader = TRUE, status='warning',
-                        selectInput('stock_comp', 'Ação', stock_list, multiple=TRUE),
+                    box(title = 'Selecione suas opções', width=12, solidHeader = TRUE, status='primary',
+                        selectInput('Estado1', 'Ação', stock_list, multiple=FALSE),
+                        selectInput('Estado2', 'Ação', stock_list, multiple=FALSE),
                         selectInput('stock_comp_ano_inicial', 'Ano Inicial', time_list, multiple=FALSE),
                         selectInput('stock_comp_ano_final', 'Ano Final', time_list, multiple=FALSE),
-                        #uiOutput("timedate_comp"),
                         actionButton('go_comp', 'Submeter')
                     )
                 ),            
+                
+                fluidRow(
+                  box(title = "Queimadas ao longo dos anos", width = 12, solidHeader = TRUE, status = "primary",
+                      plotOutput('grafo'),
+                      plotOutput('barra'),
+                      plotOutput('scat'),
+                ),
         )
+      )
     )
 )
 
 ui <- dashboardPage(
-    skin = 'red',
+    skin = 'purple',
     header, sidebar, body)
